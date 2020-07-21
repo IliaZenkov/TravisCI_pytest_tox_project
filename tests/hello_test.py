2961c3a286f936bb7af0ccf3c81d401e7d44707d
Ilia Zenkov <ilzenkov@gmail.com>
2020-07-21 08:29:06 -0400

    Delete hello_test.py

--- tests/hello_test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import find_substrings import find_substrings
-
-def hello_test(string):
-    string='hello'
-    substring.find_substrings(string)
-    assert(substring.list == ['h', 'he', 'hel', 'hell', 'hello', 'e', 'el', 'ell', 'ello', 'l', 'll', 'llo', 'l', 'lo', 'o'])
-    assert(substring.count == 15)
