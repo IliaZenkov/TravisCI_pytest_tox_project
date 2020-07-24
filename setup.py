caed936949adc867587340b04ba8608d2f9e8fdb
Ilia Zenkov <ilzenkov@gmail.com>
2020-07-24 01:32:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,9 @@
 #!/usr/bin/env python3
 
 from glob import glob
-from setuptools import find_packages, setup, Command
-from subprocess import call
-import os
+from setuptools import find_packages, setup
 
-class testcov(Command):
-    description = "runs pytest and sends report through codeclimate"
-    user_options = []
 
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        errno = call(["pytest --cov=src --durations=10 tests"], shell=True)
-        if os.getenv("TRAVIS_PULL_REQUEST") == "false":
-            call(["coverage run -m pytest"], shell=True)
-        raise SystemExit(errno)
 
 setup(name='find_substrings',
       version='1.0',
@@ -28,7 +12,6 @@ setup(name='find_substrings',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-      cmdclass={'testcov': testcov}
      )
 
 install_requires=['']
