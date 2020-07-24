dc248b166934f770ea9ae3a811bf7eb5fb8b21d1
Ilia Zenkov <ilzenkov@gmail.com>
2020-07-23 21:42:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 from glob import glob
 from setuptools import find_packages, setup, Command
+from subprocess import call
 
 class testcov(Command):
     description = "runs pytest and sends report through codeclimate"
