94bf5ea6680e449c10b304453a426eed83932054
Ilia Zenkov <ilzenkov@gmail.com>
2020-07-25 05:19:47 -0400

    Update test_substrings.py

--- tests/test_substrings.py
+++ tests/test_substrings.py
@@ -48,15 +48,26 @@ def test_substring_print(capsys):
     captured = capsys.readouterr()
     assert (
         captured.out
-        == "There are 10 or less substrings in 'test', we don't need to know what they are.\n"
+        == "There are 10 or le
+        
+        
+        
+        
+        ss substrings in 'test', we don't need to know what they are.\n"
     )
 
     string = "hello"
     substring(string)
     captured = capsys.readouterr()
     print(captured.out)
-    assert (
+    assert
+    
+    (
         captured.out
-        == "There are 15 substrings in 'hello'\n"
-        + "Substrings of 'hello': ['h', 'he', 'hel', 'hell', 'hello', 'e', 'el', 'ell', 'ello', 'l', 'll', 'llo', 'l', 'lo', 'o']\n"
+        == "There are 15 substrings
+        
+        in 'hello'\n"
+        + "Substr
+        
+        ings of 'hello': ['h', 'he', 'hel', 'hell', 'hello', 'e', 'el', 'ell', 'ello', 'l', 'll', 'llo', 'l', 'lo', 'o']\n"
     )
